955d08897715c5da032a4115a408800509700747
Hyunwoo Ryu <59816794+Chemical118@users.noreply.github.com>
2022-02-26 00:52:30 +0900

    2/26

--- pingtest.py
+++ pingtest.py
@@ -1,6 +1,7 @@
 from fake_useragent import UserAgent
 from datetime import datetime, timedelta
 from time import sleep
+from random import uniform
 import urllib.request
 
 ua = UserAgent()
@@ -14,13 +15,13 @@ nd_b = nd = datetime.now()
 with open("pinglog.txt", "w", encoding='utf-8') as f:
     f.write('Ping target : ' + tURL + '\n')
     f.write(datetime.now().strftime("%Y-%M-%D %H:%M:%S") + '\n')
-    f.write('-----<Check mode>-------\n')
+    f.write('\n----<Check mode>-------\n')
 
 while True:
     st = datetime.now()
     tar = datetime.strptime(urllib.request.urlopen(req).headers['Date'], '%a, %d %b %Y %H:%M:%S %Z')
     nd = datetime.now()
-    chk.append((nd - st).microseconds)
+    chk.append((nd - st).total_seconds())
     with open("pinglog.txt", "a", encoding='utf-8') as f:
         f.write('%d.%d %d %d.%d\n' % (st.second, st.microsecond, tar.second, nd.second, nd.microsecond))
     tar += timedelta(hours=9)  # UTC+9 맞추기
@@ -33,21 +34,22 @@ while True:
 st_b = (tar - st_b).total_seconds()
 nd = (tar - nd).total_seconds()
 atp = len(chk)
-avg_atp = sum(chk) / len(chk) / 1000
+avg_atp = sum(chk) / len(chk)
 
-print('Attempt No. : %d Avg. Ping : %fms' % (atp, avg_atp))
-print('Error : %f ~ %f\n' % (nd, st_b))
+print('Attempt No. : %d Avg. Ping : %fms' % (atp, avg_atp * 1000))
+print('Error : %f ~ %f' % (nd, st_b))
 with open("pinglog.txt", "a", encoding='utf-8') as f:
-    f.write('Attempt No. : %d Avg. Ping : %f\n' % (atp, avg_atp))
+    f.write('Attempt No. : %d Avg. Ping : %f\n' % (atp, avg_atp * 1000))
     f.write('Error : %f ~ %f\n\n' % (nd, st_b))
     f.write('-----<Accurate mode>-------\n')
 ans_int = (st_b < 0) + int(st_b)
 st_b -= ans_int
 
-acnt = 0
-ans_b = False
-while True:
-    sleep(2 - st_b - datetime.now().microsecond/1000000)
+ans_f = st_b
+ans_b = nd
+chk = []
+for _ in range(30):
+    sleep(2 - st_b - datetime.now().microsecond/1000000 + uniform(-1.6 * avg_atp, 0.4 * avg_atp))
     t_st_b = datetime.now()
     t_tar_b = datetime.strptime(urllib.request.urlopen(req).headers['Date'], '%a, %d %b %Y %H:%M:%S %Z')
     t_nd_b = datetime.now()
@@ -61,21 +63,25 @@ while True:
         f.write('%d.%d %d %d.%d\n\n' % (t_st.second, t_st.microsecond, t_tar.second, t_nd.second, t_nd.microsecond))
     # print(t_st_b, t_tar_b, t_nd_b, t_tar, t_nd)
     # print((t_nd_b - t_st_b).total_seconds(), (t_nd - t_st).total_seconds())
-    if t_tar_b.second == t_tar.second and (t_nd_b - t_st_b).total_seconds() < 0.032 and (t_nd - t_st).total_seconds() < 0.032 and t_st + timedelta(seconds=ans_int) < t_tar:
-        acnt += 1
-        print((t_tar_b - t_nd_b).total_seconds())
-        if ans_b:
-            ans_b = max(ans_b, (t_tar_b - t_nd_b).total_seconds())
-        else:
-            ans_b = (t_tar_b - t_nd_b).total_seconds()
-        if acnt == 10:
-            break
-st_b += ans_int
-ans = (ans_b + st_b) / 2
-print('\nFinal Error : %f ~ %f' % (ans_b, st_b))
-print('Error %.2fms' % ((st_b - ans_b) * 1000))
+    chk.append(0)
+    if t_st_b + timedelta(seconds=ans_int) > t_tar_b:
+        ans_f = min(ans_f, (t_tar_b - t_st_b).total_seconds() + 1)
+        chk[-1] += 1
+    else:
+        ans_b = max(ans_b, (t_tar_b - t_nd_b).total_seconds())
+    if t_st + timedelta(seconds=ans_int) > t_tar:
+        ans_f = min(ans_f, (t_tar - t_st).total_seconds() + 1)
+        chk[-1] += 1
+    else:
+        ans_b = max(ans_b, (t_tar - t_nd).total_seconds())
+
+ans = (ans_b + ans_f) / 2
+print('\nFinal Error : %f ~ %f' % (ans_b, ans_f))
+print('Error : %.2fms' % ((ans_f - ans_b) * 1000))
+print('Front : %d Middle : %d Last : %d' % (chk.count(2), chk.count(1), chk.count(0)))
 print(ans, end='')
+
 with open("pinglog.txt", "a", encoding='utf-8') as f:
-    f.write('\nFinal Error : %f ~ %f\n' % (ans_b, st_b))
-    f.write('Error %.2fms\n' % ((st_b - ans_b) * 1000))
-    f.write('%f' % ans)
+    f.write('\nFinal Error : %f ~ %f\n' % (ans_b, ans_f))
+    f.write('Error %.2fms\n' % ((ans_f - ans_b) * 1000))
+    f.write('%f' % (ans * 1000))

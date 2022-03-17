90a59a95686f08d127f32f4f7301e650aa291108
Hyunwoo Ryu <59816794+Chemical118@users.noreply.github.com>
2022-03-17 11:33:10 +0900

    Last Update
    Maybe?

--- pingtest.py
+++ pingtest.py
@@ -12,9 +12,10 @@ chk = []
 st_b = st = datetime.now()
 tar_b = tar = datetime.strptime(urllib.request.urlopen(req).headers['Date'], '%a, %d %b %Y %H:%M:%S %Z')
 nd_b = nd = datetime.now()
-with open("pinglog.txt", "w", encoding='utf-8') as f:
+fins = "pinglog/" + "{:%Y_%m_%d_%H_%M_%S}".format(nd) + ".txt"
+with open(fins, "w", encoding='utf-8') as f:
     f.write('Ping target : ' + tURL + '\n')
-    f.write(datetime.now().strftime("%Y-%M-%D %H:%M:%S") + '\n')
+    f.write(nd.strftime("%Y-%M-%D %H:%M:%S") + '\n')
     f.write('\n----<Check mode>-------\n')
 
 while True:
@@ -22,7 +23,7 @@ while True:
     tar = datetime.strptime(urllib.request.urlopen(req).headers['Date'], '%a, %d %b %Y %H:%M:%S %Z')
     nd = datetime.now()
     chk.append((nd - st).total_seconds())
-    with open("pinglog.txt", "a", encoding='utf-8') as f:
+    with open(fins, "a", encoding='utf-8') as f:
         f.write('%d.%d %d %d.%d\n' % (st.second, st.microsecond, tar.second, nd.second, nd.microsecond))
     tar += timedelta(hours=9)  # UTC+9 맞추기
     if (st - st_b).total_seconds() < 0.025 and tar.second != tar_b.second:
@@ -38,18 +39,18 @@ avg_atp = sum(chk) / len(chk)
 
 print('Attempt No. : %d Avg. Ping : %fms' % (atp, avg_atp * 1000))
 print('Error : %f ~ %f' % (nd, st_b))
-with open("pinglog.txt", "a", encoding='utf-8') as f:
+with open(fins, "a", encoding='utf-8') as f:
     f.write('Attempt No. : %d Avg. Ping : %f\n' % (atp, avg_atp * 1000))
     f.write('Error : %f ~ %f\n\n' % (nd, st_b))
     f.write('-----<Accurate mode>-------\n')
 ans_int = (st_b < 0) + int(st_b)
-st_b -= ans_int
 
 ans_f = st_b
 ans_b = nd
+st_b -= ans_int
 chk = []
-for _ in range(60):
-    sleep(2 - st_b - datetime.now().microsecond/1000000 + uniform(-1.6 * avg_atp, 0.4 * avg_atp))
+for _ in range(int(input("반복 횟수를 입력하세요 : "))):
+    sleep(2 - st_b - datetime.now().microsecond/1000000 + uniform(-1.2 * avg_atp, 0.2 * avg_atp))
     t_st_b = datetime.now()
     t_tar_b = datetime.strptime(urllib.request.urlopen(req).headers['Date'], '%a, %d %b %Y %H:%M:%S %Z')
     t_nd_b = datetime.now()
@@ -58,7 +59,7 @@ for _ in range(60):
     t_nd = datetime.now()
     t_tar_b += timedelta(hours=9)  # UTC+9 맞추기
     t_tar += timedelta(hours=9)  # UTC+9 맞추기
-    with open("pinglog.txt", "a", encoding='utf-8') as f:
+    with open(fins, "a", encoding='utf-8') as f:
         f.write('%d.%d %d %d.%d\n' % (t_st_b.second, t_st_b.microsecond, t_tar_b.second, t_nd_b.second, t_nd_b.microsecond))
         f.write('%d.%d %d %d.%d\n\n' % (t_st.second, t_st.microsecond, t_tar.second, t_nd.second, t_nd.microsecond))
     # print(t_st_b, t_tar_b, t_nd_b, t_tar, t_nd)
@@ -79,9 +80,22 @@ ans = (ans_b + ans_f) / 2
 print('\nFinal Error : %f ~ %f' % (ans_b, ans_f))
 print('Error : %.2fms' % ((ans_f - ans_b) * 1000))
 print('Front : %d times, Middle : %d times, Last : %d times' % (chk.count(2), chk.count(1), chk.count(0)))
-print(ans, end='')
 
-with open("pinglog.txt", "a", encoding='utf-8') as f:
+with open(fins, "a", encoding='utf-8') as f:
     f.write('\nFinal Error : %f ~ %f\n' % (ans_b, ans_f))
     f.write('Error %.2fms\n' % ((ans_f - ans_b) * 1000))
     f.write('%f' % (ans * 1000))
+
+with open("macro.js", "r", encoding='utf-8') as f:
+    code_list = f.readlines()
+    ploc_list = [(ind, val) for ind, val in enumerate(code_list) if "const lcpt =" in val]
+    if len(ploc_list) != 1:
+        print('macro.js를 확인해주세요\n')
+        for i in ploc_list:
+            print("%d번째 줄 : %s" % (i[0] + 1, i[1].lstrip()))  # 왼쪽 공백 자동 제거
+        f.close()
+        exit(len(ploc_list))
+    code_list[ploc_list[0][0] + 1] = "        " + str(round(ans * 1000, 6)) + ";\n"
+with open("macro.js", "w", encoding='utf-8') as f:
+    f.write(''.join(code_list))
+print('정상적으로 처리되었습니다 Answer : %.2fms' % (ans * 1000), end='')
